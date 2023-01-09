3cb694921731d6d3af1250889607c7e2518348c7
Vinícius Tôrres <vinicius.carlosoficial@outlook.com>
2023-01-09 18:57:48 -0300

    Refatoração

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const botao = document.querySelectorAll('.botao-carrosel');
-let cards = document.querySelectorAll('.card-carrosel');
-let pontoInicial = 0;
-const maxItens = cards.length;
-
-botao.forEach(botao => {
-    botao.addEventListener('click', () => {
-        const isLeft = botao.classList.contains('botao-esquerdo');
-        if (isLeft) {
-            pontoInicial--
-        } else {
-            pontoInicial++
-        }
-        if(pontoInicial >= maxItens) {
-            pontoInicial = 0;
-        } else if (pontoInicial < 0) {
-            pontoInicial = maxItens - 1
-        }
-
-        cards[pontoInicial].scrollIntoView({
-            inline: "center",
-            behavior: "smooth",
-            block : "nearest"
-        })
-    })
-})
-
 const botaoCopiar = document.getElementById("botaoCopiar");
 botaoCopiar.addEventListener('click', ()=> {
     if(navigator.clipboard.writeText(botaoCopiar.value)) {
@@ -47,11 +20,8 @@ function escrever(elemento) {
         }, 150 * index)
     })
 }
-
 const titulo = document.getElementById('titulo__secao-principal');
 escrever(titulo)
-
-
 
 let botaoMenu = document.getElementById('botao-menu');
 botaoMenu.addEventListener('click', menuAtivo);
